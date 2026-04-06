12048d108ed46853696208d46126b24f16652eb6
paulkyev <paul.kyev.saire@gmail.com>
2026-04-06 17:38:24 -0500

    add new coment more structure

--- demo-cosmosdb/src/main/java/com/example/democosmosdb/collections/ListExamples.java
+++ demo-cosmosdb/src/main/java/com/example/democosmosdb/collections/ListExamples.java
@@ -2,32 +2,44 @@ package com.example.democosmosdb.collections;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class List {
+public class ListExamples {
     public static void main(String[] args) {
+        //importa la posicion y permite elementos duplicdos
 
+        //arraylist:
         // los arraylist son rapido para consulta (for, forEach,etc) pero lento con las inserciones
-        //permite elementos duplicados
-        java.util.List<String> names = new ArrayList<>();
+        List<String> names = new ArrayList<>();
         names.add("Paul");
         names.add("Paul");
         names.remove(1); // al eliminar este se corre noma todo, juana ahora seria del indice 1
         names.add("Juana");
         names.add("Remino");
 
-//        for (int i = 0; i<names.size(); i++) {
-//            System.out.println(i + ": " + names.get(i));
-//        }
+        for (int i = 0; i<names.size(); i++) {
+            if (names.get(i).equals("Juana")){
+                names.remove(i);
+            }
+        } // este seria eliminar con un for
 
-        //muy rapido para eliminar y guardar elementos, lento en las consultas
-        // permite elementos repetidos
+        //List<String> newNames = names.stream().filter(p -> !"Juana".equals(p)).toList(); //este seria la forma de eliminar con stream
+
+
+        //System.out.println(names);
+
+
+
+        //LinkedList:
+        //muy rapido para eliminar y guardar elementos, lento en las consultas, usan punteros internamente
         LinkedList<String> namesLinked = new LinkedList<>();
 
         namesLinked.add("Juan");
         namesLinked.add("Remiro");
         namesLinked.add("Max");
-        namesLinked.addFirst("Pedro");
-        namesLinked.addLast("Gillian");
+        namesLinked.addFirst("Pedro"); // este metodo solo lo tiene el linkedList
+        namesLinked.addLast("Gillian"); // este metodo solo lo tiene el linkedList
         namesLinked.add("Julian");
 
 //        for (int i = 0; i<namesLinked.size(); i++) {
@@ -42,7 +54,7 @@ public class List {
         urls.add("stackoverflow.com");
         urls.addFirst("youtube.com");
 
-        System.out.println(urls.getFirst());
-        System.out.println(urls.getLast());
+//        System.out.println(urls.getFirst());
+//        System.out.println(urls.getLast());
     }
 }
